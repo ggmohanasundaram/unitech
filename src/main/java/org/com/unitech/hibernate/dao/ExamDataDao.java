9b6d2a8f2c8430f25ef7619a753a541a3752e0c2
Mohanasundaram Govindarajan <ggmohan@gmail.com>
2016-09-24 22:56:27 +1000

    added method to get specific filed

--- src/main/java/org/com/unitech/hibernate/dao/ExamDataDao.java
+++ src/main/java/org/com/unitech/hibernate/dao/ExamDataDao.java
@@ -1,5 +1,6 @@
 package org.com.unitech.hibernate.dao;
 
+import org.com.unitech.exceptions.DaoException;
 import org.com.unitech.hibernate.model.ExamData;
 
 import java.util.List;
@@ -8,5 +9,7 @@ import java.util.List;
  * Created by MohanDivya on 9/24/2016.
  */
 public interface ExamDataDao {
-    List<ExamData> getInformation(int id) throws Exception;
+    List<ExamData> getInformation(int id) throws DaoException;
+
+    List<String> getSpecificInformation(int id, String field) throws DaoException;
 }
